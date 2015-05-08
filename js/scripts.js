29dbc56ac1fc73458e1d322bf27d9d79fc068a2f
Randy Wiafe <wiafe23@gmail.com>
2015-05-08 14:55:50 -0600

    Page
    asdfk asdgs

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.screen').unslider({
+		speed: 500,
+		delay: 3000,
+		complete: function() {},
+		keys: true,
+		dots: true,
+		fluid: false
+		});
+    console.log('helo');
+});
